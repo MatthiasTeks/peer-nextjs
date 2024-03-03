05d836ae06ed0eb4ad712d4af268909fb042729e
Matthias <matthias.vimbert@gmail.com>
2024-03-03 15:38:25 +0100

    init repo

---
+++ peer-server/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const { ExpressPeerServer } = require("peer");
+const express = require("express");
+const cors = require("cors");
+
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello world!");
+});
+
+const PORT = 9000;
+
+app.use(express.static("public"));
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  allow_discovery: true,
+});
+
+app.use("/myapp", peerServer);
+
+server.listen(PORT, () => {
+  console.log(`PeerJS server running on port ${PORT}`);
+});
